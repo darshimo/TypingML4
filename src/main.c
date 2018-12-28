5ca1eafc00c88e695730f8d15a995d51869d68bb
darshimo <s4qedth5y@gmail.com>
2018-12-28 11:17:52 +0900

    ファイル名を指定するように変更

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include "param.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define DEBUG
 #ifdef DEBUG
-#include <stdio.h>
 void debugCncl(Cncl *,int);
 #endif
 
@@ -17,11 +17,21 @@ void writeCncl(Cncl *, int);
 int main(int argc, char *argv[]){
     if(argc!=2)error("arg is not correct.\n");
 
+    char *str = (char *)malloc(sizeof(char)*500);
+    char *filename = (char *)malloc(sizeof(char)*20);
+    sprintf(filename,"../problem/%s",argv[1]);
+    FILE *fp;
+    if((fp = fopen(filename,"r"))==NULL){
+        printf("fopen error.");
+        exit(EXIT_FAILURE);
+    }
+    fgets(str,500,fp);
+
 
 #ifdef DEBUG
     printf("read start.\n");
 #endif
-    Cncl *cncl_ob = readCncl(argv[1]);
+    Cncl *cncl_ob = readCncl(str);
 #ifdef DEBUG
     printf("read complete.\n\n");
 #endif
